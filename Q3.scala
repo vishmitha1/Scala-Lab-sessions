a9ff1fe773476a364217f0e63d8b2330aa676104
visal <visaluni2@gmail.com>
2023-06-13 08:37:02 +0530

    change 4/3

--- Q3.scala
+++ Q3.scala
@@ -1,7 +1,7 @@
 import scala.math.Pi
 object test{
   def Volume(radius:Double)={
-    4/3*Pi*radius*radius*radius;
+    Pi*radius*radius*radius*4/3;
   }
   def main(args:Array[String])={
     print(Volume(5.0));
